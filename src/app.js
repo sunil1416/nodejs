db5b06014c8be60e78a506e1e740e6d5c5ffb1e4
Trijal Varma <trijal@trijals-imac.mynetworksettings.com>
2024-05-17 09:20:10 -0400

    initial

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>
+{
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('HelloWorld');
+
+
+});
+
+server.listen(3000,'127.0.0.1', () => {
+    console.log('Server running...');
+});
